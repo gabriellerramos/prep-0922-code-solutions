763992bc8d9888400277737bd935947eeb65ea8b
Gabrielle Ramos <gabireid22@gmail.com>
2022-09-28 01:10:41 +0000

    completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: 'Gabrielle Ramos',
+  lastName: 'Ramos',
+  hobby: 'baking',
+  job: 'student',
+  previousJob: 'sales'
+};
+console.log(person);
+var fullName = ' The persons name is: Gabrielle Ramos';
+console.log(fullName);
+var job = 'The persons current job is: student';
+console.log(job);
+var previousJob = 'The persons previous job is: sales';
+console.log(previousJob);
